75899f7ab805807c4fa6e3413dffd99ddcd867b0
Giacomo Glotzer <gglotzer@rockefeller.edu>
2025-06-20 11:42:25 -0400

    Renamed visualization.py to l2.py.

--- src/crantpy/viz/__init__.py
+++ src/crantpy/viz/__init__.py
@@ -8,10 +8,12 @@ __getattr__, __dir__, __all__ = lazy_loader.attach(
     },
     submod_attrs={
         'visualization': [
+            'find_anchor_loc',
             'get_l2_chunk_info',
             'get_l2_info',
         ],
     },
 )
 
-__all__ = ['get_l2_chunk_info', 'get_l2_info', 'visualization']
+__all__ = ['find_anchor_loc', 'get_l2_chunk_info', 'get_l2_info',
+           'visualization']
