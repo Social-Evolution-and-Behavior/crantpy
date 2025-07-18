f216d6bdde4b866e96af6082629dd17444280432
Giacomo Glotzer <gglotzer@rockefeller.edu>
2025-07-18 09:50:29 -0400

    Ensure that positions are integers in get_all_seatable_annotations

--- src/crantpy/queries/__init__.py
+++ src/crantpy/queries/__init__.py
@@ -13,12 +13,9 @@ __getattr__, __dir__, __all__ = lazy_loader.attach(
             'get_adjacency',
             'get_synapses',
         ],
-<<<<<<< HEAD
-=======
         'get_adjacency': [
             'get_adjacency',
         ],
->>>>>>> 57865ba6a93806b5c525642ab5ddaa145a275f89
         'neurons': [
             'F',
             'NeuronCriteria',
